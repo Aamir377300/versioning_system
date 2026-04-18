8e00f3fbd91b78f1db0d4680b0bf416c80477237
Aamir377300 <belalaamirkhan@gmail.com>
2026-04-18 09:00:10 +0530

    deploy

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(
     DEBUG=(bool, False)
 )
-environ.Env.read_env(BASE_DIR / '.env')
+environ.Env.read_env(BASE_DIR / '.env', overwrite=False)
 
 
 # Quick-start development settings - unsuitable for production
